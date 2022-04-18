3b82aff86ce8f9ed3a6af775e6f7ecf9355e6b44
michaelrae0 <mdrae0@gmail.com>
2022-04-18 13:22:39 -0500

    add branch env

--- background.js
+++ background.js
@@ -3,16 +3,23 @@
  */
 const LOCAL = {
   prefix: 'local',
-  domain: '://titan.local.expandshare.com',
+  match: '://titan.local.expandshare.com',
+}
+const BRANCH = {
+  prefix: 'branch',
+  match: '/g-branches/',
 }
 const DEV = {
   prefix: 'dev',
-  domain: '://titan.dev.expandshare.com',
+  match: '://titan.dev.expandshare.com',
+  sub_envs: [BRANCH],
 }
 const PROD = {
   prefix: 'prod',
-  domain: '://app.expandshare.com',
+  match: '://app.expandshare.com',
 }
+
+// root level envs
 const ENVIRONMENTS = [LOCAL, DEV, PROD]
 
 
@@ -21,24 +28,35 @@ const ENVIRONMENTS = [LOCAL, DEV, PROD]
  */
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   if (changeInfo.status == 'complete') {
+    const env = getEnvironment(tabId, tab, ENVIRONMENTS)
 
-    ENVIRONMENTS.some(env => { // loop envs
-      if (tab.url.includes(env.domain)) { // match env
-
-        if (!tab.title.startsWith(env.prefix)) { // add prefix if not already added
-          chrome.scripting.executeScript({
-            target: { tabId },
-            function: addPrefixToTitle,
-            args: [env, tab], // add variables to script scope
-          })
-        }
-
-        return true
-      }
-    })
-
+    if (env && !tab.title.includes(env.prefix)) { // add prefix if not already added
+      chrome.scripting.executeScript({
+        target: { tabId },
+        function: addPrefixToTitle,
+        args: [env, tab], // add variables to script scope
+      })
+    }
   }
 })
+
+
+/**
+ * Returns active environment
+ * Checks root environments, then sub environments
+ */
+function getEnvironment(tabId, tab, envs) {
+  for (const env of envs) { // loop envs
+    if (tab.url.includes(env.match)) { // match env
+      if (env.sub_envs) { // return sub_env if match
+        const sub = getEnvironment(tabId, tab, env.sub_envs)
+        if (sub) return sub
+      }
+
+      return env // return env
+    }
+  }
+}
 
 
 /**
